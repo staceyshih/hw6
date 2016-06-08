21359b8b8613515dc4f9ab053e8f8709922c38b8
staceyshih <ka9515hebe520777@gmail.com>
2016-06-09 06:15:58 +0800

    init

--- js/main.js
+++ js/main.js
@@ -15,22 +15,6 @@ ImageDealer.REF = firebase;
 var currentUser ;
 
 
- window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1758594857708619',
-      xfbml      : true,
-      version    : 'v2.6'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
 
 /*
     分為三種使用情形：
